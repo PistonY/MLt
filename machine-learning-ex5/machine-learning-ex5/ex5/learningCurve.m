83f1feb17cd4da5b8713890caa968584dc5683a0
Piston Yang <soulfd@icloud.com>
2018-01-07 17:24:33 +0800

    done ex5

--- machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
@@ -54,9 +54,13 @@ error_val   = zeros(m, 1);
 % ---------------------- Sample Solution ----------------------
 
 
-
-
-
+for i = 1:m,
+  x = X(1:i,:);
+  yt = y(1:i);
+  [theta] = trainLinearReg(x, yt, lambda);
+  error_train(i) = linearRegCostFunction(x, yt, theta, 0);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 % -------------------------------------------------------------
